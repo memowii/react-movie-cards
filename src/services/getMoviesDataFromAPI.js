e3a8c326bcdf2b76d960eafb0a15c1244f855e3a
memowii <memogl25@gmail.com>
2020-09-23 19:14:18 -0500

    Fetch movies

--- src/services/getMoviesDataFromAPI.js
+++ src/services/getMoviesDataFromAPI.js
@@ -2,50 +2,46 @@ const fs = require("fs");
 const axios = require("axios");
 const querystring = require("querystring");
 
-function x(apikey) {
-
-  return function x2(obj) {
+function setApi(apikey) {
+  return function (obj) {
     obj.apikey = apikey;
+
     obj["t"] = obj["title"];
     delete obj["title"];
+
     obj["y"] = obj["year"];
     delete obj["year"];
 
-    return querystring.stringify(obj);
+    return "?" + querystring.stringify(obj);
   };
 }
 
-// const API_URL = "http://www.omdbapi.com/?apikey=e0d2283&t=Reservoir+Dogs&y=1992";
+// This is an example of how a request should look:
+// http://www.omdbapi.com/?apikey=e0d2283&t=Reservoir+Dogs&y=1992
 const API_URL = "http://www.omdbapi.com";
 const API_KEY = "e0d2283";
 axios.defaults.baseURL = API_URL;
 axios.defaults.timeout = 1200;
 
+const formApiQuery = setApi(API_KEY);
+
 const rawData = fs.readFileSync(__dirname + "/imdbMoviesFromHTMLSource.json");
 const moviesData = JSON.parse(rawData);
-
 const movies3 = moviesData.slice(0, 3);
 
-const xx = x(API_KEY);
-console.log(xx(movies3[0]));
-
-// process.exit();
-
 async function getMoviesData(simpleMovies) {
-  const moviesData = simpleMovies.map(async (movie) => {});
+  try {
+    const responds = await Promise.all(
+      moviesData
+        .map((movie) => formApiQuery(movie))
+        .map((query) => axios.get(query))
+    );
+
+    const data = JSON.stringify(responds.map((respond) => respond.data));
+    fs.writeFileSync(__dirname + "/imdbMoviesFromAPI.json", data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-axios
-  // .get("?apikey=e0d2283&t=Reservoir+Dogs&y=1992")
-  .get("?apikey=e0d2283&t=The%20Shawshank%20Redemption&y=1994")
-  .then(function (response) {
-    // handle success
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
-// .then(function () {
-//   // always executed
-// });
+getMoviesData();
